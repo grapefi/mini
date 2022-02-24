3001adc1a5ee6ef6e684ef31ba039082c8eea501
grapefi <96745303+grapefi@users.noreply.github.com>
2022-02-24 23:35:01 +1100

    Update formatBalance.ts

--- src/utils/formatBalance.ts
+++ src/utils/formatBalance.ts
@@ -18,7 +18,7 @@ export const getDisplayBalance = (
 };
 
 export const getFullDisplayBalance = (balance: BigNumber, decimals = 18, isTruncated = false) => {
-  return getDisplayBalance(balance, decimals, 10, isTruncated);
+  return getDisplayBalance(balance, decimals, 6, isTruncated);
 };
 
 export function getBalance(balance: BigNumber, decimals = 18): number {
