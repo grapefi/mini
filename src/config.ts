c21432156a7c146597bc3d1ce9dc0c39e94467af
grapefi <96745303+grapefi@users.noreply.github.com>
2022-02-26 02:17:40 +1100

    a

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ import { BankInfo } from './tomb-finance';
 const configurations: { [env: string]: Configuration } = {
 
   production: {
-    chainId: 250,
+    chainId: ChainId.MAINNET,
     networkName: 'Fantom Opera Mainnet',
     ftmscanUrl: 'https://ftmscan.com',
     defaultProvider: 'https://rpc.ftm.tools/',
